52c0dc09e979e12e5b0c11f85d4124c680f13d23
teekingtv2 <devteeking@gmail.com>
2024-12-19 11:28:44 +0100

    server4

--- src/routes/admin/general-routes.js
+++ src/routes/admin/general-routes.js
@@ -5,6 +5,6 @@ const {
 } = require("../../controllers/admin/admin-auth-controller");
 const router = express.Router();
 
-router.get("/all-users", verifyAdminLoginToken, fetchAllUsers);
+router.get("/all-users", fetchAllUsers);
 
 module.exports = router;
